a8a6dda27b09139948a728fd9cebb64f5f23f346
Habib Max <habib.max00@gmail.com>
2025-10-17 00:48:14 +0300

    while silmukka esm valimis
    seuraava tehtavat

--- while_silmukko/ESM_while/Esm5.py
+++ while_silmukko/ESM_while/Esm5.py
@@ -1,7 +1,15 @@
 #5. while + else
-i = 4
+i = 0
 while i < 3:
     print(i)
     i += 1
 else:
     print("Silmukka päättyi normaalisesti")
+#➡️ else-lohko suoritetaan vain jos silmukka ei pääty break-komennolla.➡️ else-lohko suoritetaan vain jos silmukka ei pääty break-komennolla.v➡️ else-lohko suoritetaan vain jos silmukka ei pääty break-komennolla.
+#⚠️ Varoitus
+
+#Jos unohdat muuttaa ehtoa silmukan sisällä, voit saada ikuisen silmukan:
+i = 0
+while i < 5:
+    print(i)
+    # i += 1 puuttuu! -> ohjelma ei koskaan lopu
